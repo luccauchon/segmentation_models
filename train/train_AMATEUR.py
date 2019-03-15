d165c86f84a3999b979755a6d05519d75bab545a
CJ3272 <cauchon.luc@ireq.ca>
2019-03-15 11:10:54 -0400

    Lâche pas mon Gérard.

--- train/train_AMATEUR.py
+++ train/train_AMATEUR.py
@@ -1,10 +1,9 @@
 import os
 os.environ['basedir_a'] = 'C:/temp2/'
 
-import train
 
-from train.data.Generators import AmateurDataFrameDataGenerator
-import train.data.Generators as generators
+from luccauchon.data.Generators import AmateurDataFrameDataGenerator
+import luccauchon.data.Generators as generators
 
 from segmentation_models import Unet
 from segmentation_models.backbones import get_preprocessing
@@ -12,8 +11,23 @@ from segmentation_models.losses import bce_jaccard_loss
 from segmentation_models.metrics import iou_score
 
 
-df_train, df_val = generators.amateur_train_val_split('G:/AMATEUR/segmentation/22FEV2019/GEN_segmentation/', number_elements=32)
+df_train, df_val = generators.amateur_train_val_split('G:/AMATEUR/segmentation/22FEV2019/GEN_segmentation/', number_elements=16)
 
-
+dim_image = (256, 256, 3)
 BACKBONE = 'resnet34'
-preprocess_input = get_preprocessing(BACKBONE)
+BACKBONE = 'inceptionresnetv2'
+#preprocess_input = get_preprocessing(BACKBONE)
+# preprocess input
+#x_train = preprocess_input(x_train)
+#x_val = preprocess_input(x_val)
+
+# define model
+model = Unet(BACKBONE, encoder_weights='imagenet')
+model.compile('Adam', loss=bce_jaccard_loss, metrics=[iou_score])
+model.summary()
+train_generator = AmateurDataFrameDataGenerator(df_train, batch_size=4, dim_image=dim_image)
+val_generator = AmateurDataFrameDataGenerator(df_val, batch_size=4, dim_image=dim_image)
+
+model.fit_generator(generator=train_generator, steps_per_epoch=None, epochs=10, verbose=1, callbacks=None,
+                    validation_data=val_generator, validation_steps=None, class_weight=None, max_queue_size=10,
+                    workers=2, use_multiprocessing=False, shuffle=True, initial_epoch=0)
