4d065a131c729bf92007f3eeed4ece60a6d3fa03
cj3272 <cauchon.luc@ireq.ca>
2019-03-29 12:50:40 -0400

    Lâche pas mon Gérard.

--- train/train_AMATEUR.py
+++ train/train_AMATEUR.py
@@ -14,17 +14,26 @@ from segmentation_models.losses import cce_jaccard_loss
 from segmentation_models.metrics import iou_score
 from segmentation_models.metrics import jaccard_score
 
+import keras
+
+BACKBONE = 'resnet34'
+BACKBONE = 'inceptionresnetv2'
+BACKBONE = 'seresnet152'
+
 class_ids = [0, 1]
 dim_image = (256, 256, 3)
+
+
+if BACKBONE == 'seresnet152':
+    dim_image = (384, 384, 3)
+
 df_train, df_val = generators.amateur_train_val_split(dataset_dir='G:/AMATEUR/segmentation/22FEV2019/GEN_segmentation/', class_ids=class_ids, number_elements=16)
 
 train_generator = AmateurDataFrameDataGenerator(df_train, classes_id=class_ids, batch_size=4, dim_image=dim_image)
 val_generator = AmateurDataFrameDataGenerator(df_val, classes_id=class_ids, batch_size=4, dim_image=dim_image)
 
 
-BACKBONE = 'resnet34'
-BACKBONE = 'inceptionresnetv2'
-BACKBONE = 'seresnet152'
+
 
 # preprocess input
 # from segmentation_models.backbones import get_preprocessing
@@ -42,7 +51,16 @@ model = Linknet(BACKBONE, classes=len(class_ids), encoder_weights='imagenet')
 model.compile('Adam', loss=bce_jaccard_loss, metrics=[iou_score])
 model.summary()
 
-model.fit_generator(generator=train_generator, steps_per_epoch=None, epochs=10, verbose=1, callbacks=None,
+modelCheckpoint = keras.callbacks.ModelCheckpoint(filepath='segmod_weights.{epoch:02d}-{val_loss:.4f}.hdf5',
+                                                  monitor='val_loss',
+                                                  verbose=0, save_best_only=False, save_weights_only=False,
+                                                  mode='auto', period=1)
+reduceLROnPlateau = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=7, verbose=1,
+                                                      mode='auto', min_delta=0.001, cooldown=0, min_lr=10e-7)
+
+
+model.fit_generator(generator=train_generator, steps_per_epoch=None, epochs=10, verbose=1,
+                    callbacks=[reduceLROnPlateau, modelCheckpoint],
                     validation_data=val_generator, validation_steps=None, class_weight=None, max_queue_size=10,
                     workers=2, use_multiprocessing=False, shuffle=True, initial_epoch=0)
 
